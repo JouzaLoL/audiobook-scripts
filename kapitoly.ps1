0ad745e312cf0a320adae7542ff7eb5116ceb4fe
Josef Vacek <vacekj@outlook.com>
2018-11-29 21:48:09 +0100

    add scripts to source control

---
+++ kapitoly.ps1
@@ -0,0 +1,32 @@
+$folder= (Get-Item -Path ".\").FullName;
+$com = (New-Object -ComObject Shell.Application).NameSpace($folder);
+$lengthattribute = 27;
+for($i = 0; $i -lt 64; $i++) {
+	$name = $com.GetDetailsOf($com.Items, $i)
+	if ($name -eq 'Length') { $lengthattribute = $i}
+}
+
+($kapitoly = $com.Items() |
+	Where-Object{-not($_.IsFolder)} |
+	ForEach-Object {
+		[PSCustomObject]@{
+			Name = $_.Name
+			Length = [timespan]$com.GetDetailsOf($_, $lengthattribute)
+		}
+    }
+)
+
+$timestamps = @'
+Kapitoly:
+
+'@;
+
+$currentTime = New-TimeSpan;
+foreach ($item in $kapitoly) {
+    $timestamps = $timestamps + $currentTime.ToString("h\:mm\:ss") + " - " + $item.Name + "`n";
+    $currentTime = $currentTime.Add($item.Length);
+}
+
+$timestamps > kapitoly.txt
+
+    
---
+++ render.ps1
@@ -0,0 +1,56 @@
+function Render {
+    param (
+        [string]$Image, [string]$Audio, [string]$Folder
+    )
+
+    $Folder = $Folder -replace '\s',''
+
+    $renderCommand = "ffmpeg -y -loop 1 -framerate 1 -i ""$($Image)"" $($Audio) -vf ""pad=ceil(iw/2)*2:ceil(ih/2)*2"" -c:v libx264 -preset veryslow -crf 0 -c:a copy -shortest ""$($renderFolder + '\' + $Folder).mp4""";
+    
+    Invoke-Expression $renderCommand;
+}
+
+# TODO: Ignore folders without mp3s or jpegs
+
+# Where to put rendered files
+$renderFolder = "./Rendered";
+
+$renderFolderExists = Get-ChildItem -Directory -Filter $renderFolder;
+
+if(!$renderFolderExists.Exists) {
+    New-Item -ItemType Directory -Force -Path $renderFolder;
+}
+
+$folders = Get-ChildItem -Recurse -Directory;
+
+foreach ($folder in $folders) {
+    if($folder.FullName.Contains("Rendered")) {
+        continue;
+    }
+
+    # get image
+    $Image = Get-ChildItem $folder.FullName -include ('*.jpg', '*.png') -recurse | ForEach-Object { $_.FullName };
+    if ($Image.Length -eq 0) {
+        continue;
+    }
+
+    # get mp3(s)
+    # $alreadyConcat = Get-ChildItem -Path $folder.FullName -Recurse -Filter "*MP3WRAP*"
+    # if($alreadyConcat.Exists) {
+    #     $alreadyConcat.Delete();
+    # }
+
+    $mp3s = Get-ChildItem $folder.FullName -include ('*.mp3', '*.m4a') -recurse | ForEach-Object { $_.FullName };
+    if ($mp3s.Length -gt 1) {
+        $mp3s = $mp3s | Sort-Object;
+        $mp3s = $mp3s | ForEach-Object { "-i ""$($_)"" "};
+        $mp3s = $mp3s -join "";
+    }
+
+    if ($mp3s.Length -eq 0) {
+        continue;
+    }
+
+    Render $Image $mp3s $folder
+    
+}
